7c14d185c7092a835cdb1baed2a66782156e899d
조미은/디지털자산센터추진팀 <mieun.jo@hyundai-autoever.com>
2024-04-22 15:49:33 +0900

    add the post section of the feed component

--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -1,8 +1,31 @@
 import { SparklesIcon } from "@heroicons/react/24/outline";
 import React from "react";
 import Input from "./Input";
+import Post from "./Post";
+import { PostProps } from "../types";
 
 const Feed = () => {
+  const posts: PostProps[] = [
+    {
+      id: "1",
+      name: "Ayo Kim",
+      username: "codewithayo.kim",
+      userImg: "https://avatars.githubusercontent.com/u/71052435?v=4",
+      img: "https://plus.unsplash.com/premium_photo-1669048777088-0e091701ac54?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "nice view!",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Ayo Kim",
+      username: "codewithayo.kim",
+      userImg: "https://avatars.githubusercontent.com/u/71052435?v=4",
+      img: "https://plus.unsplash.com/premium_photo-1669048778846-9524559b011c?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "nice view!",
+      timestamp: "2 days ago",
+    },
+  ];
+
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       {/* Home Title */}
@@ -15,6 +38,11 @@ const Feed = () => {
 
       {/* Profile and Textarea */}
       <Input />
+
+      {/* Posts */}
+      {posts.map((post: PostProps) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 };
---
+++ app/types.ts
@@ -0,0 +1,9 @@
+export interface PostProps {
+  id: string;
+  name: string;
+  username: string;
+  userImg: string;
+  img: string;
+  text: string;
+  timestamp: string;
+}
